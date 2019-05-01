eb66a89e02a2bf86e7081c46a4b99a207e2608d3
Alfredo <alfredo.perezp93@gmail.com>
2019-05-01 20:00:26 +0200

    Hammer and Digglet Base
    - Hammer animations and script logic almost finished.
    - Digglet model separation.
    - Digglet animations and base script logic.

--- Assets/Scripts/Mole.cs
+++ Assets/Scripts/Mole.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Mole : MonoBehaviour
 {
+
+    private Animator m_anim;
+
+    enum Hats {Mexican, King, Pirate };
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_anim = GetComponent<Animator>();
+    }
+
+    void Hide() {
+        m_anim.SetBool("isHide", true);
+    }
+
+    void Unhide(Hats hat, bool isShainy) {
+        m_anim.SetBool("isHide", false);
     }
 
     // Update is called once per frame
