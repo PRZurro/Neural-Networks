05b46ea2d0fc87ffc7f22754940a6637fbe1d755
Pablo Rodríguez <pablorodriguezzurro@gmail.com>
2019-05-02 22:42:47 +0200

    All existing classes commented
    - Added a new class to the Artificial Neural Network.

--- Assets/Scripts/Hammer.cs
+++ Assets/Scripts/Hammer.cs
@@ -17,29 +17,43 @@ public class Hammer : MonoBehaviour
 
     void Update()
     {
+        // If it has a target move to it
         if (m_isMoving)
         {
             MoveTo();
         }
     }
 
+    /// <summary>
+    /// Do an animation of a hit
+    /// </summary>
     void Hit()
     {
         m_anim.SetBool("isHitting", true);
         Invoke("GoIdle", 0.2f);
     }
 
+    /// <summary>
+    /// Set the flags to move to the target and hit
+    /// </summary>
+    /// <param name="target"></param>
     public void HitOn(Vector3 target)
     {
         m_target = target;
         m_isMoving = true;
     }
 
+    /// <summary>
+    /// Set the animation state to idle
+    /// </summary>
     void GoIdle()
     {
         m_anim.SetBool("isHitting", false);
     }
 
+    /// <summary>
+    /// Move this object to the target and trigger the hit animation when the target is reached
+    /// </summary>
     void MoveTo()
     {
         transform.position = Vector3.MoveTowards(transform.position, m_target, MovementSpeed * Time.deltaTime);
